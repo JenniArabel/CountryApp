8a84884f0487f31bddc1172238ddd209d6bdaa63
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-08-11 06:38:28 -0300

    Create layout component

--- countryApp/src/app/app.routes.ts
+++ countryApp/src/app/app.routes.ts
@@ -2,16 +2,16 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 
 export const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: HomePageComponent,
+  // },
   {
     path: '',
-    component: HomePageComponent,
-  },
-  {
-    path: 'country',
     loadChildren: () => import('./country/country.routes'),//.then(m => m.countryRoutes),
   },
-  {
-    path:'**',
-    redirectTo: ''
-  }
+  // {
+  //   path:'**',
+  //   redirectTo: ''
+  // }
 ];
