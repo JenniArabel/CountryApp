7054ab61be65d84fe325840a5e1d008e9165fea6
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-08-28 07:49:17 -0300

    API connection part 1

--- countryApp/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ countryApp/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-import { SearchInputComponent } from "../../components/search-input/search-input.component";
-import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { Component, inject } from '@angular/core';
+import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'by-capital-page',
   imports: [SearchInputComponent, CountryListComponent],
   templateUrl: './by-capital-page.component.html',
 })
-export class ByCapitalPageComponent { }
+export class ByCapitalPageComponent {
+  countryService = inject(CountryService);
+
+  onSearch(value: string){
+    console.log({value});
+  }
+}
